683e7c4345a7d82ef6ab984c3f21af1e8e83e7e8
renhongbin <1239279122@qq.com>
2020-11-25 18:52:33 +0800

    v01-登录功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+// Message导入弹框组件
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.prototype.$message = Message
